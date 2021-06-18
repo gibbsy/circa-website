6e5944216d382ba55de0b2d9b7513bd32eca3376
Andy Gillon <aawgillon@gmail.com>
2021-06-18 15:19:41 +0100

    Privacy page

--- pages/legal/_slug.vue
+++ pages/legal/_slug.vue
@@ -1,33 +1,35 @@
 <template>
   <div ref="scroll-legal" class="page-wrapper ancillary-page legals">
+    <div id="sticky-logo-target"></div>
     <transition name="fade" appear mode="out-in">
       <div
         ref="logo-peel"
         class="circa-logo logo-corner-left"
         aria-label="circa Logo"
         data-scroll
+        data-scroll-sticky="true"
+        data-scroll-target="#sticky-logo-target"
       >
         <nuxt-link to="/">
           <logo />
         </nuxt-link>
       </div>
     </transition>
-    <section id="services-wrapper" class="services__container" data-scroll>
+    <section
+      id="legals-wrapper"
+      class="generic__container legals__container"
+      data-scroll
+    >
       <div
-        class="services-texture-bg pull-right"
+        class="generic-texture-bg pull-right"
         data-scroll
-        data-scroll-direction="horizontal"
-        data-scroll-speed="2"
+        data-scroll-sticky="true"
+        data-scroll-target="#sticky-logo-target"
       >
-        <div
-          class="inner-texture"
-          data-scroll
-          data-scroll-direction="horizontal"
-          data-scroll-speed="-2"
-        ></div>
+        <div class="inner-texture"></div>
       </div>
 
-      <div class="services__content">
+      <div class="generic__content">
         <transition name="fade" appear mode="out-in">
           <block-content
             :class-name="'legals__text text-block--full-width'"
@@ -60,16 +62,63 @@ export default {
     return { pageData: pageData[0] };
   },
   data() {
-    return {};
+    return {
+      scroll: {
+        update: () => {
+          console.log("scroll not initialized");
+        },
+        destroy: () => {
+          console.log("scroll not initialized destroy");
+        },
+      },
+    };
   },
-  mounted() {},
-
+  mounted() {
+    this.init();
+    document.body.style.setProperty("--bg-root", "#ffffff");
+  },
+  beforeDestroy() {
+    console.log("DESTROY SLUG");
+    window.removeEventListener("resize", this.handleResize);
+    this.scroll.destroy();
+    this.scroll = null;
+  },
   methods: {
     init() {
-      // this.initScroll();
+      this.initScroll();
     },
     urlFor(source) {
       return urlBuilder.image(source);
+    },
+    initScroll() {
+      const el = this.$refs["scroll-legal"];
+      this.scroll = new this.LocomotiveScroll({
+        el,
+        smooth: true,
+        getDirection: true,
+      });
+      setTimeout(() => {
+        this.updateScroll();
+      }, 500);
+      this.initScrollEvents();
+    },
+    initScrollEvents() {
+      window.addEventListener("resize", this.handleResize);
+    },
+    updateScroll() {
+      console.log("update scroll");
+      this.scroll.update();
+      // ScrollTrigger.refresh();
+    },
+    scrollTo(target, options) {
+      this.scroll.scrollTo(target, options);
+    },
+    handleResize() {
+      console.log("Resize");
+      clearTimeout(this.resizeTimeout);
+      this.resizeTimeout = setTimeout(() => {
+        this.updateScroll();
+      }, 250);
     },
   },
 };
